bd15aa9cd65bcc312179909b6f9781e1a5646026
SavenokNikolay <savenok.kolya.nikolay@gmail.com>
2023-07-19 15:56:32 +0300

    add set

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const buttonCount = 5;
-const timeout = 1000;
+const timeout = 500;
 const shadow = 'shadow'
 
 for (let i = 1; i <= buttonCount; i++) {
@@ -18,21 +18,25 @@ const buttonsPromisses = buttonsElements.map(
    }))
 
 Promise.all(buttonsPromisses.filter((_, index) => index % 2 === 0))
-.then(() => {
-   alert('Всі непарні кнопки натиснуті!');
- });
+   .then(() => {
+      setTimeout(() => {
+         alert('Всі непарні кнопки натиснуті!');
+      }, timeout);
+   });
 
- Promise.all(buttonsPromisses.filter((_, index) => index % 2 !== 0))
- .then(() => {
-   alert('Всі парні кнопки натиснуті!');
- });
+Promise.all(buttonsPromisses.filter((_, index) => index % 2 !== 0))
+   .then(() => {
+      setTimeout(() => {
+         alert('Всі парні кнопки натиснуті!');
+      }, timeout);
+   });
 
 Promise.all(buttonsPromisses)
    .then((answer) => {
       if (answer) {
          setTimeout(() => {
-           alert('Всі кнопки натиснуті!');
-         }, timeout);
-       }
+            alert('Всі кнопки натиснуті!');
+         }, 2500);
+      }
    })
 
